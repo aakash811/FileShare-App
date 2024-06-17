019904910770302befef5b3c7f9f4b3ff6b75a82
aakash811 <borseaakash082004@gmail.com>
2024-06-17 09:19:36 +0530

    fix

--- app/(dashboard)/(routes)/file-preview/[fileId]/page.js
+++ app/(dashboard)/(routes)/file-preview/[fileId]/page.js
@@ -8,7 +8,7 @@ import React, { useEffect, useState } from "react";
 import FileInfo from "./_components/FileInfo";
 import FileShareForm from "./_components/FileShareForm";
 
-function FilePreview({ params, file }) {
+function FilePreview({ params }) {
   const db = getFirestore(app);
   const [fileInfo, setFileInfo] = useState();
 
