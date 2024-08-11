de22e71acb8f9a53936b8558a5accf3a7307c38b
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2024-08-11 14:25:47 +0530

    Update Header.js

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -53,7 +53,7 @@ function Header() {
               <div className="sm:flex sm:gap-4">
                 <a
                   className="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-600"
-                  href="/files"
+                  href="/upload"
                 >
                   Get Started
                 </a>
