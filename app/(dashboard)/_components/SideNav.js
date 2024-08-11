745f00bec68e17780b558877e8687f5695eb3113
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2024-08-11 14:24:04 +0530

    Update SideNav.js

--- app/(dashboard)/_components/SideNav.js
+++ app/(dashboard)/_components/SideNav.js
@@ -1,12 +1,9 @@
 "use client";
 import { File, Shield, Upload } from "lucide-react";
 import Image from "next/image";
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 function Sidenav() {
-  const [mounted, setMounted] = useState(false);
-  const router = useRouter(); // Initialize the router
   const menuList = [
     {
       id: 1,
@@ -22,24 +19,12 @@ function Sidenav() {
     },
     {
       id: 3,
-      name: "Upgrade",
+      name: "Upgade",
       icon: Shield,
       path: "/upgrade",
     },
   ];
   const [activeIndex, setActiveIndex] = useState(0);
-
-  useEffect(() => {
-    setMounted(true); // Set mounted to true after component mounts
-  }, []);
-
-  const handleNavigation = (path, index) => {
-    setActiveIndex(index);
-    if (mounted) {
-      router.push(path); // Navigate to the specified path
-    }
-  };
-
   return (
     <div className="shadow-sm border-r h-full">
       <div className="p-5">
@@ -49,11 +34,10 @@ function Sidenav() {
       <div className="flex flex-col w-full">
         {menuList.map((item, index) => (
           <button
-            key={item.id}
             className={`flex gap-2 p-4 px-6 hover:bg-gray-100 w-full text-gray-500 ${
               activeIndex === index ? "bg-blue-50 text-primary" : null
             }`}
-            onClick={() => handleNavigation(item.path, index)} // Use the handleNavigation function
+            onClick={() => setActiveIndex(index)}
           >
             <item.icon />
             <h2>{item.name}</h2>
