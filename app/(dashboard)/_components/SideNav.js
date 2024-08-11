59fb8f6aa59c4f7dfc3f21a10c77d6870e3322bb
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2024-08-11 14:17:33 +0530

    Update SideNav.js

--- app/(dashboard)/_components/SideNav.js
+++ app/(dashboard)/_components/SideNav.js
@@ -2,8 +2,10 @@
 import { File, Shield, Upload } from "lucide-react";
 import Image from "next/image";
 import React, { useState } from "react";
+import { useRouter } from "next/router";
 
 function Sidenav() {
+  const router = useRouter();  // Initialize the router
   const menuList = [
     {
       id: 1,
@@ -19,12 +21,18 @@ function Sidenav() {
     },
     {
       id: 3,
-      name: "Upgade",
+      name: "Upgrade",
       icon: Shield,
       path: "/upgrade",
     },
   ];
   const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleNavigation = (path, index) => {
+    setActiveIndex(index);
+    router.push(path);  // Navigate to the specified path
+  };
+
   return (
     <div className="shadow-sm border-r h-full">
       <div className="p-5">
@@ -34,10 +42,11 @@ function Sidenav() {
       <div className="flex flex-col w-full">
         {menuList.map((item, index) => (
           <button
+            key={item.id}
             className={`flex gap-2 p-4 px-6 hover:bg-gray-100 w-full text-gray-500 ${
               activeIndex === index ? "bg-blue-50 text-primary" : null
             }`}
-            onClick={() => setActiveIndex(index)}
+            onClick={() => handleNavigation(item.path, index)}  // Use the handleNavigation function
           >
             <item.icon />
             <h2>{item.name}</h2>
