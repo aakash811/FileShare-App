e33a02d37ceb17a71d2e80c8ca384579938d91a2
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2024-08-11 14:14:22 +0530

    Update Header.js

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -23,7 +23,7 @@ function Header() {
                 <li>
                   <a
                     className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
+                    href="/upload"
                   >
                     Upload
                   </a>
